4bf2d21ecff1e8db106c721d6f2a175367f760cf
RootDevelop <robbert@rootdevelop.nl>
2016-10-03 18:24:16 +0200

    Update AdminController.cs

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using MyWedding.Data;
 using MyWedding.Models;
+using MyWedding.Models.Enums;
 
 
 namespace MyWedding.Controllers
